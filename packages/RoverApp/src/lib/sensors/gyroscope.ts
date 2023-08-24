43dcb1b97a0af787969e48db02bff636414a7c16
Jérôme Mestres <jerome@mestres.fr>
2023-08-24 17:38:16 +0200

    extract gyro sensor logic into dedicated class

---
+++ packages/RoverApp/src/lib/sensors/gyroscope.ts
@@ -0,0 +1,43 @@
+// inspired from SEN-MPU6050_Manual (https://joy-it.net/files/files/Produkte/SEN-MPU6050/SEN-MPU6050_Beispielcode.zip)
+import { readWord, writeByte } from '@robot/rover-app/helpers/i2c-promise.js';
+import { getGyroY, getGyroX, getGyroZ } from '@robot/rover-app/helpers/gyro.js';
+
+import type { I2CBus } from 'i2c-bus';
+
+const gyro_addr = 0x68; // address of gyroscope module (SEN-MPU6050)
+const acceleration_scale_factor = 16384.0; // scale factors of accelerometer
+
+export default class GyroscopeSensor {
+  i2cbus: I2CBus;
+
+  constructor(i2cbus: I2CBus) {
+    this.i2cbus = i2cbus;
+  }
+
+  async init() {
+    await writeByte(this.i2cbus, gyro_addr, 0x6b, 0);
+  }
+
+  async getTemperatureSensor() {
+    const temperature = await readWord(this.i2cbus, gyro_addr, 0x41);
+    return Number((temperature / 340 + 36.53).toFixed(1));
+  }
+
+  async getGyroSensor() {
+    // reading accelerometer values
+    const accelX = await readWord(this.i2cbus, gyro_addr, 0x3b);
+    const accelY = await readWord(this.i2cbus, gyro_addr, 0x3d);
+    const accelZ = await readWord(this.i2cbus, gyro_addr, 0x3f);
+
+    // scaled accelerometer values
+    const accelXScaled = accelX / acceleration_scale_factor;
+    const accelYScaled = accelY / acceleration_scale_factor;
+    const accelZScaled = accelZ / acceleration_scale_factor;
+
+    return {
+      x: getGyroY(accelXScaled, accelYScaled, accelZScaled),
+      y: getGyroX(accelXScaled, accelYScaled, accelZScaled),
+      z: getGyroZ(accelXScaled, accelYScaled, accelZScaled),
+    };
+  }
+}
--- packages/RoverApp/src/services/external-sensors.ts
+++ packages/RoverApp/src/services/external-sensors.ts
@@ -1,14 +1,9 @@
-// inspired from SEN-MPU6050_Manual (https://joy-it.net/files/files/Produkte/SEN-MPU6050/SEN-MPU6050_Beispielcode.zip)
 import Service from './-base.js';
 import { openSync } from 'i2c-bus';
-import { getGyroY, getGyroX, getGyroZ } from '@robot/rover-app/helpers/gyro.js';
-import { readWord, writeByte } from '@robot/rover-app/helpers/i2c-promise.js';
 import { external_sensor_update_interval } from '@robot/rover-app/const.js';
+import GyroscopeSensor from '@robot/rover-app/lib/sensors/gyroscope.js';
 
 import type { I2CBus } from 'i2c-bus';
-
-const gyro_addr = 0x68; // address of gyroscope module (SEN-MPU6050)
-const acceleration_scale_factor = 16384.0; // scale factors of accelerometer
 
 class ExternalSensorsService extends Service {
   static serviceName = 'external-sensors';
@@ -16,13 +11,18 @@ class ExternalSensorsService extends Service {
   i2cbus: I2CBus;
   internal?: NodeJS.Timeout;
 
+  gyro: GyroscopeSensor;
+
   constructor(...args: ConstructorParameters<typeof Service>) {
     super(...args);
     this.i2cbus = openSync(1);
+    this.gyro = new GyroscopeSensor(this.i2cbus);
   }
 
   async init() {
-    await this.initGyro();
+    // Init sensors
+    await this.gyro.init();
+
     this.internal = setInterval(
       this.sendExtenalSensorEvent.bind(this),
       external_sensor_update_interval,
@@ -31,44 +31,13 @@ class ExternalSensorsService extends Service {
 
   // Aggregate all external sensor values & send a single "externalSensor" event
   async sendExtenalSensorEvent() {
-    const temperature = await this.getTemperatureSensor();
-    const gyro = await this.getGyroSensor();
+    const temperature = await this.this.gyro.getTemperatureSensor();
+    const gyro = await this.this.gyro.getGyroSensor();
 
     this.eventBroker.emit('externalSensor', {
       gyro,
       temperature,
     });
-  }
-
-  async readWord(address: number, cmd: number) {
-    return await readWord(this.i2cbus, address, cmd);
-  }
-
-  async initGyro() {
-    await writeByte(this.i2cbus, gyro_addr, 0x6b, 0);
-  }
-
-  async getTemperatureSensor() {
-    const temperature = await this.readWord(gyro_addr, 0x41);
-    return Number((temperature / 340 + 36.53).toFixed(1));
-  }
-
-  async getGyroSensor() {
-    // reading accelerometer values
-    const accelX = await this.readWord(gyro_addr, 0x3b);
-    const accelY = await this.readWord(gyro_addr, 0x3d);
-    const accelZ = await this.readWord(gyro_addr, 0x3f);
-
-    // scaled accelerometer values
-    const accelXScaled = accelX / acceleration_scale_factor;
-    const accelYScaled = accelY / acceleration_scale_factor;
-    const accelZScaled = accelZ / acceleration_scale_factor;
-
-    return {
-      x: getGyroY(accelXScaled, accelYScaled, accelZScaled),
-      y: getGyroX(accelXScaled, accelYScaled, accelZScaled),
-      z: getGyroZ(accelXScaled, accelYScaled, accelZScaled),
-    };
   }
 }
 
