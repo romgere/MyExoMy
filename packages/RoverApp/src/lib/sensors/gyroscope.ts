1864d21bd0acd9e59906181e69de4cafd8e3fef0
Jérôme Mestres <jerome@mestres.fr>
2023-08-25 11:57:32 +0200

    refactor gyroscope sensor code

--- packages/RoverApp/src/lib/sensors/gyroscope.ts
+++ packages/RoverApp/src/lib/sensors/gyroscope.ts
@@ -1,34 +1,41 @@
 // inspired from SEN-MPU6050_Manual (https://joy-it.net/files/files/Produkte/SEN-MPU6050/SEN-MPU6050_Beispielcode.zip)
-import { readWord, writeByte } from '@robot/rover-app/helpers/i2c-promise.js';
 import { getGyroY, getGyroX, getGyroZ } from '@robot/rover-app/helpers/gyro.js';
 import { openSync } from 'i2c-bus';
 
-import type { I2CBus } from 'i2c-bus';
+import type { PromisifiedBus } from 'i2c-bus';
 
 const gyro_addr = 0x68; // address of gyroscope module (SEN-MPU6050)
 const acceleration_scale_factor = 16384.0; // scale factors of accelerometer
 
 export default class GyroscopeSensor {
-  i2cbus: I2CBus;
+  i2cbus: PromisifiedBus;
 
   constructor() {
-    this.i2cbus = openSync(1);
+    this.i2cbus = openSync(1).promisifiedBus();
   }
 
   async init() {
-    await writeByte(this.i2cbus, gyro_addr, 0x6b, 0);
+    await this.i2cbus.writeByte(gyro_addr, 0x6b, 0);
   }
 
   async getTemperatureSensor() {
-    const temperature = await readWord(this.i2cbus, gyro_addr, 0x41);
+    const buffer = Buffer.alloc(2);
+    await this.i2cbus.readI2cBlock(gyro_addr, 0x41, 2, buffer);
+
+    const temperature = buffer.readInt16BE();
+
     return Number((temperature / 340 + 36.53).toFixed(1));
   }
 
   async getGyroSensor() {
     // reading accelerometer values
-    const accelX = await readWord(this.i2cbus, gyro_addr, 0x3b);
-    const accelY = await readWord(this.i2cbus, gyro_addr, 0x3d);
-    const accelZ = await readWord(this.i2cbus, gyro_addr, 0x3f);
+    const buffer = Buffer.alloc(6);
+
+    await this.i2cbus.readI2cBlock(gyro_addr, 0x3b, 6, buffer);
+
+    const accelX = buffer.readInt16BE();
+    const accelY = buffer.readInt16BE(2);
+    const accelZ = buffer.readInt16BE(4);
 
     // scaled accelerometer values
     const accelXScaled = accelX / acceleration_scale_factor;
