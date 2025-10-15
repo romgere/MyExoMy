96ed845cba386ad5a4724b26b65138fef3e79863
Mestres Jérôme <jerome@mestres.fr>
2025-10-15 22:22:08 +0200

    chore: add color to temperature badges

--- packages/ControlCenter/app/components/sensors-status.ts
+++ packages/ControlCenter/app/components/sensors-status.ts
@@ -6,4 +6,26 @@ interface SensorStatusArgs {}
 
 export default class SensorStatus extends Component<SensorStatusArgs> {
   @service declare roverSensor: RoverSensorService;
+
+  get piTemperatureBadgeVariant() {
+    const t = this.roverSensor.piTemperature;
+    if (t < 50) {
+      return 'success';
+    } else if (t < 65) {
+      return 'warning';
+    }
+
+    return 'danger';
+  }
+
+  get bodyTemperatureBadgeVariant() {
+    const t = this.roverSensor.bodyTemperature;
+    if (t < 30) {
+      return 'success';
+    } else if (t < 40) {
+      return 'warning';
+    }
+
+    return 'danger';
+  }
 }
