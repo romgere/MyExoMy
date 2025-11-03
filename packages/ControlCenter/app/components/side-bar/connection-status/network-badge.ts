55e1fcf72bd62fad75919bfdc08c45c7aae44777
Mestres Jérôme <jerome@mestres.fr>
2025-11-03 10:00:01 +0100

    chore: update side panel section color & add some spacing to GPS badges

--- packages/ControlCenter/app/components/side-bar/connection-status/network-badge.ts
+++ packages/ControlCenter/app/components/side-bar/connection-status/network-badge.ts
@@ -24,6 +24,7 @@ export default class NetworkBadgeComponent extends Component<Args> {
 
     return 'wifi';
   }
+
   get badgeVariant() {
     if (!this.roverSensor.currentIwData) {
       return 'neutral';
